4988633304af8b84ff367533b8caebba445de5e7
profButch <butch.wesley+prof@gmail.com>
2023-08-21 13:15:54 -0400

    initial commit

---
+++ Assets/IT4080/Editor/IT4080Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+
+[InitializeOnLoadAttribute]
+public static class IT4080Menu
+{
+    public const string VERSION = "0.1.0";
+
+    [MenuItem("IT4080/About")]
+    private static void MnuAbout()
+    {
+        Debug.Log($"IT4080 Menu Version {VERSION}");
+    }
+
+    [MenuItem("IT4080/Window")]
+    private static void MnuWindow()
+    {
+        //It4080.It4080TheWindow window = new It4080.It4080TheWindow()
+        var window = EditorWindow.GetWindow<It4080.It4080TheWindow>(utility: true, title: "IT4080 The Flame Thrower", focus: true);
+        window.ShowUtility();
+    }
+}
---
+++ Assets/IT4080/Editor/It4080TheWindow.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.PackageManager.UI;
+using UnityEngine.UIElements;
+using System.IO;
+
+
+/*
+ * Some links
+ * https://stackoverflow.com/questions/3791103/c-sharp-continuously-read-file
+ * 
+ */
+
+
+namespace It4080
+{
+    public class It4080TheWindow : EditorWindow {
+        string myString = "Hello World";
+        bool groupEnabled;
+        bool myBool = true;
+        float myFloat = 1.23f;
+
+        public It4080TheWindow() {
+        }
+
+
+        public void CreateGUI() {
+            Label lbl = new Label("Hello");
+            rootVisualElement.Add(lbl);
+
+            TextField txt = new TextField();
+            txt.multiline = true;           
+            rootVisualElement.Add(txt);
+            txt.value = FileToText("/Users/profbutch/temp/unity_builds/TheBuild_1.log");
+        }
+
+
+        void OnGUI() {
+            //GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+            //myString = EditorGUILayout.TextField("Text Field", myString);
+
+            //groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+            //myBool = EditorGUILayout.Toggle("Toggle", myBool);
+            //myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+            //EditorGUILayout.EndToggleGroup();
+        }
+
+
+        private string FileToText(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+            string toReturn = reader.ReadToEnd();
+            reader.Close();
+            return toReturn;
+        }
+    }
+
+}
